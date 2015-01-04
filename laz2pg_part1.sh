208d220af899ecdd8f063aee324919fe23f3ad0d
Stefan Ziegler <edi.gonzales@gmail.com>
2015-01-04 19:38:58 +0100

    laz2pg.sh

---
+++ laz2pg_part1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BASEPATH=/home/stefan/Geodaten/lidar_part1/
+
+for FILE in ${BASEPATH}*.laz
+do
+  BASENAME=$(basename $FILE .laz)
+  echo "Processing: $FILE"
+  
+  laszip.exe -i $FILE -o ${BASEPATH}/${BASENAME}.las 
+  sed s/MY_LAZ_FILE/${BASENAME}.las/g laz2pg_part1_template.xml > laz2pg_part1.xml
+  pdal pipeline laz2pg_part1.xml
+  rm laz2pg_part1.xml
+  rm ${BASEPATH}/${BASENAME}.las
+
+done
--- queries.sql
+++ queries.sql
@@ -44,3 +44,38 @@ FROM av_lidar_2014.lidar;
 
 
 
+-> Table  -> Cluster...
+
+
+
+CREATE TABLE av_lidar_2014.lidar_patches AS
+SELECT
+  pa::geometry(Polygon, 21781) AS geom,
+  PC_PatchAvg(pa, 'Z') AS elev_avg,
+  PC_PatchMax(pa, 'Z') AS elev_max,
+  PC_PatchMin(pa, 'Z') AS elev_min,
+  id
+FROM av_lidar_2014.lidar;
+
+
+ALTER TABLE av_lidar_2014.lidar_patches ADD PRIMARY KEY (id);
+
+ALTER TABLE av_lidar_2014.lidar_patches OWNER TO stefan;
+GRANT ALL ON av_lidar_2014.lidar_patches TO stefan;
+GRANT SELECT ON av_lidar_2014.lidar_patches TO mspublic;
+
+
+CREATE INDEX idx_av_lidar_2014_lidar_patches_id
+  ON av_lidar_2014.lidar_patches
+  USING btree
+  (id);
+
+
+CREATE INDEX idx_av_lidar_2014_lidar_patches_geom
+  ON av_lidar_2014.lidar_patches
+  USING gist
+  (geom);
+
+
+ALTER TABLE av_lidar_2014.lidar_patches ALTER COLUMN geom SET not null;   
+CLUSTER idx_av_lidar_2014_lidar_patches_geom ON av_lidar_2014.lidar_patches; 
